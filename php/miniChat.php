d705f6d4ece5373db1adf53136163deb2688a827
Angel-smith974 <elfedelanuit974@hotmail.fr>
2019-10-16 13:18:43 +0400

    Premier commit

---
+++ php/miniChat.php
@@ -0,0 +1,5 @@
+<form action="miniChat_post.php" method='post'>
+    <p class='nom'>Pseudo <input type="text"></p>
+    <p class='message'>Message <input type="text"></p>
+    <input type="submit" value="Envoyez">
+</form>
---
+++ php/miniChat_post.php
@@ -0,0 +1,9 @@
+<?php
+// Effectuer ici la requête qui insère le message :
+echo $_GET['nom'];
+
+
+
+// Puis rediriger vers minichat.php comme ceci :
+header('Location: miniChat.php');
+?>
