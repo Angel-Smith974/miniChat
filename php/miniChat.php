bffd4553bfb0669ffa64183059d81928a8ea4559
Angel-smith974 <elfedelanuit974@hotmail.fr>
2019-10-17 08:44:18 +0400

    commit aide morgan

--- php/miniChat.php
+++ php/miniChat.php
@@ -1,5 +1,29 @@
-<form action="miniChat_post.php" method='post'>
-    <p class='nom'>Pseudo <input type="text"></p>
-    <p class='message'>Message <input type="text"></p>
-    <input type="submit" value="Envoyez">
+<form action="miniChat_post.php" method="post">
+    <p>Pseudo : <input type="text" name="pseudo" id="pseudo"></p>
+    <p>Message : <input type="text" name="message" id="message"></p>
+    <input type="submit" value="Envoyer">
 </form>
+
+<?php
+try
+{
+	$bdd = new PDO('mysql:host=localhost;dbname=mini_chat;charset=utf8', 'root', '');
+}
+catch (Exception $e)
+{
+        die('Erreur : ' . $e->getMessage());
+}
+
+$reponse = $bdd->query('SELECT pseudo,message FROM chat');
+
+while ($donnees = $reponse->fetch())
+{
+?>
+    <strong><?php echo $donnees['pseudo']?></strong><br />
+    <p><?php echo $donnees['message']?></p>
+<?php
+}
+
+$reponse->closeCursor(); // Termine le traitement de la requête
+
+?>
--- php/miniChat_post.php
+++ php/miniChat_post.php
@@ -1,8 +1,26 @@
 <?php
+
+try
+{
+	$bdd = new PDO('mysql:host=localhost;dbname=mini_chat;charset=utf8', 'root', '');
+}
+catch (Exception $e)
+{
+        die('Erreur : ' . $e->getMessage());
+}
+
+
 // Effectuer ici la requête qui insère le message :
-echo $_GET['nom'];
+//var_dump($_POST);
+$pseu = $_POST["pseudo"];
+$mess = $_POST["message"];
 
+$req = $bdd->prepare('INSERT INTO chat(pseudo, message) VALUES(:pseudo, :message)');
 
+$req->execute(array(
+	'pseudo' => $pseu,
+	'message' => $mess
+	));
 
 // Puis rediriger vers minichat.php comme ceci :
 header('Location: miniChat.php');
